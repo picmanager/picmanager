e5b4c04f5eec1b1382dd4fa36aee3b6def9f1c6b
Vasileios Ntoufoudis <info@ntoufoudis.com>
2024-07-22 13:30:40 +0300

    Login/Logout Functionality #6

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
