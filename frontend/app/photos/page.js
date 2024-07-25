36cd1bd968bb95169728600a0b762f48f718c2bb
Vasileios Ntoufoudis <info@ntoufoudis.com>
2024-07-25 14:12:14 +0300

    Refactor Upload Frontend

--- frontend/app/photos/page.js
+++ frontend/app/photos/page.js
@@ -1,59 +1,62 @@
 'use client'
 
-import { useRouter } from "next/navigation";
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {useMedia} from "../../hooks/media";
-import Image from "next/image";
 
 const FileUploadForm = () => {
 
-    const router = useRouter()
+    const {uploadFile} = useMedia({})
 
-    const {upload} = useMedia({
-        middleware: 'auth',
-        redirectIfAuthenticated: '/dashboard',
-    })
-
-    const [file, setFile] = useState("");
+    const [file, setFile] = useState(null);
+    const [status, setStatus] = useState();
     const [errors, setErrors] = useState([])
 
-    const submitForm = async event => {
-        // event.preventDefault()
-        upload({
+    const handleFileChange = (e) => {
+        const file = (e.target).files?.[0];
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+        uploadFile({
             file,
-            setErrors
+            setStatus,
+            setErrors,
         })
-    }
-
-    const handleFileChange = (event) => {
-        setFile(event.target.files[0]);
-        submitForm(file)
-    };
-
-    function showUpload() {
-            document.getElementById("uploadFile").click();
+        console.log({file})
     }
 
     return (
-        <div>
-        <button
-            id="uploadButton"
-            className="m-auto relative w-96 h-80 border rounded-xl border-gray-200 bg-white"
-            onClick={showUpload}
-        >
-            <img src="/upload.png" alt="upload" className="w-full rounded-xl"/>
-            <div className="absolute bottom-0 left-0 mx-12 mb-4">
-                <h2>CLICK TO UPLOAD YOUR FIRST PHOTO</h2>
+        <>
+            <div>
+                <h2 className="text-2xl font-bold">Upload File</h2>
+                <div className="font-medium text-sm text-green-600 mb-4">
+                    {status}
+                </div>
+                {/*<button*/}
+                {/*    id="uploadButton"*/}
+                {/*    className="m-auto relative w-96 h-80 border rounded-xl border-gray-200 bg-white"*/}
+                {/*    onClick={showUpload}*/}
+                {/*>*/}
+                {/*    <img src="/upload.png" alt="upload" className="w-full rounded-xl"/>*/}
+                {/*    <div className="absolute bottom-0 left-0 mx-12 mb-4">*/}
+                {/*        <h2>CLICK TO UPLOAD YOUR FIRST PHOTO</h2>*/}
+                {/*    </div>*/}
+                {/*    /!*<h2>File Upload</h2>*!/*/}
+                {/*</button>*/}
+                {/*<form className="hidden">*/}
+                <label htmlFor="file" className="sr-only">
+                    Choose a file
+                </label>
+                <input
+                    id="file"
+                    type="file"
+                    onChange={handleFileChange}
+                />
+                {/*<button type="submit">Upload</button>*/}
+                {/*</form>*/}
             </div>
-            {/*<h2>File Upload</h2>*/}
-        </button>
-    {/*<form className="hidden">*/}
-        <input id="uploadFile" type="file" className="hidden" onChange={handleFileChange}/>
-        {/*<button type="submit">Upload</button>*/}
-    {/*</form>*/}
-        </div>
-)
-    ;
+        </>
+    )
+        ;
 };
 
 export default FileUploadForm
--- frontend/hooks/media.js
+++ frontend/hooks/media.js
@@ -1,57 +1,25 @@
-import useSWR from "swr";
 import axios from "../config/config";
-import { useEffect } from "react";
-import { useParams, useRouter } from "next/navigation";
 
-export const useMedia = ({ middleware, redirectIfAuthenticated } = {}) => {
-    const router = useRouter()
-    const params = useParams()
-
-    const { data: user, error, mutate } = useSWR('/api/user', () =>
-        axios
-            .get('/api/user')
-            .then(res => res.data)
-            .catch(error => {
-                if (error.response.status !== 409) throw error
-
-                router.push('/verify-email')
-            }),
-    )
-
-    const csrf = () => axios.get('/sanctum/csrf-cookie')
-
-    const upload = async ({ setErrors, ...props }) => {
-        await csrf()
-
+export const useMedia = () => {
+    const uploadFile = ({file, setStatus, setErrors}) => {
         setErrors([])
+        setStatus(null)
 
         axios
-            .post('/api/upload', props, {
+            .post('/api/upload', {file}, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             })
-            .then(() => mutate())
-            .catch(error => {
-                if (error.response.status !== 422) throw error
-
-                setErrors(error.response.data.errors)
+            .then(function (response) {
+                setStatus(response.data.message);
             })
+            .catch(function (error) {
+                setErrors(error);
+            });
     }
 
-    // useEffect(() => {
-    //     if (middleware === 'guest' && redirectIfAuthenticated && user)
-    //         router.push(redirectIfAuthenticated)
-    //     if (
-    //         window.location.pathname === '/verify-email' &&
-    //         user?.email_verified_at
-    //     )
-    //         router.push(redirectIfAuthenticated)
-    //     if (middleware === 'auth' && error) logout()
-    // }, [user, error]);
-
     return {
-        user,
-        upload,
+        uploadFile,
     }
 }
